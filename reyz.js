784932151ce1e34fada1b9c6bba47483ec632e7a
Reyz <91467569+user133883@users.noreply.github.com>
2022-05-18 22:50:40 +0800

    Update reyz.js

--- reyz.js
+++ reyz.js
@@ -33,19 +33,6 @@ const client = new Client({
 var wit = moment().tz('Asia/Jayapura').format('HH:mm:ss');
 var wita = moment().tz('Asia/Makassar').format('HH:mm:ss');
 var wib = moment().tz('Asia/Jakarta').format('HH:mm:ss');
-
-var express = require('express');
-var app     = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
-});
-
  
 client.on('qr', (qr) => {
     qrcode.generate(qr, {small: true});
