49d0bdd80aa8a97fc1018ac92e4cb01dc57f4b34
Reyz <91467569+user133883@users.noreply.github.com>
2022-05-20 19:35:41 +0800

    Update reyz.js

--- reyz.js
+++ reyz.js
@@ -337,38 +337,49 @@ client.on('message', async (msg) => {
 client.on('message', async (msg) => {
     if(msg.body.startsWith('!pint ')) {
         let cha = msg.getChat();
-        if (cha.isGroup){
-            msg.reply('fitur pinterest akan berfungsi di obrolan pribadi')  
-        }else {
-            try{
-                const link = msg.body.slice(6);
-                msg.reply('*wait kak* permintaan sedang di proses.....')
-                const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
-                await delay(500)
-                hx.pinterest(link)
-                .then(result => {
-                    const hasil = (random_item(result))
-                    console.log(hasil)
-                    const start = async function(a, b) {
-                        const media = await MessageMedia.fromUrl(hasil)
-                        const chat = await msg.getChat()
-                        await chat.sendMessage(media, {caption: link})
-                    }
-                    start();
-                }).catch(err => {
-                    msg.reply('saya tidak menemukan apapun ):')
-                });
-            }catch (err) {
-                try {
-                    msg.reply('opss terjadi error')
+        const link = msg.body.slice(6);
+        msg.reply('*wait* permintaan sedang di proses !')
+        axios.get('https://id.pinterest.com/search/pins/?autologin=true&q=' + link, {
+			headers: {
+			"cookie" : "_auth=1; _b=\"AVna7S1p7l1C5I9u0+nR3YzijpvXOPc6d09SyCzO+DcwpersQH36SmGiYfymBKhZcGg=\"; _pinterest_sess=TWc9PSZHamJOZ0JobUFiSEpSN3Z4a2NsMk9wZ3gxL1NSc2k2NkFLaUw5bVY5cXR5alZHR0gxY2h2MVZDZlNQalNpUUJFRVR5L3NlYy9JZkthekp3bHo5bXFuaFZzVHJFMnkrR3lTbm56U3YvQXBBTW96VUgzVUhuK1Z4VURGKzczUi9hNHdDeTJ5Y2pBTmxhc2owZ2hkSGlDemtUSnYvVXh5dDNkaDN3TjZCTk8ycTdHRHVsOFg2b2NQWCtpOWxqeDNjNkk3cS85MkhhSklSb0hwTnZvZVFyZmJEUllwbG9UVnpCYVNTRzZxOXNJcmduOVc4aURtM3NtRFo3STlmWjJvSjlWTU5ITzg0VUg1NGhOTEZzME9SNFNhVWJRWjRJK3pGMFA4Q3UvcHBnWHdaYXZpa2FUNkx6Z3RNQjEzTFJEOHZoaHRvazc1c1UrYlRuUmdKcDg3ZEY4cjNtZlBLRTRBZjNYK0lPTXZJTzQ5dU8ybDdVS015bWJKT0tjTWYyRlBzclpiamdsNmtpeUZnRjlwVGJXUmdOMXdTUkFHRWloVjBMR0JlTE5YcmhxVHdoNzFHbDZ0YmFHZ1VLQXU1QnpkM1FqUTNMTnhYb3VKeDVGbnhNSkdkNXFSMXQybjRGL3pyZXRLR0ZTc0xHZ0JvbTJCNnAzQzE0cW1WTndIK0trY05HV1gxS09NRktadnFCSDR2YzBoWmRiUGZiWXFQNjcwWmZhaDZQRm1UbzNxc21pV1p5WDlabm1UWGQzanc1SGlrZXB1bDVDWXQvUis3elN2SVFDbm1DSVE5Z0d4YW1sa2hsSkZJb1h0MTFpck5BdDR0d0lZOW1Pa2RDVzNySWpXWmUwOUFhQmFSVUpaOFQ3WlhOQldNMkExeDIvMjZHeXdnNjdMYWdiQUhUSEFBUlhUVTdBMThRRmh1ekJMYWZ2YTJkNlg0cmFCdnU2WEpwcXlPOVZYcGNhNkZDd051S3lGZmo0eHV0ZE42NW8xRm5aRWpoQnNKNnNlSGFad1MzOHNkdWtER0xQTFN5Z3lmRERsZnZWWE5CZEJneVRlMDd2VmNPMjloK0g5eCswZUVJTS9CRkFweHc5RUh6K1JocGN6clc1JmZtL3JhRE1sc0NMTFlpMVErRGtPcllvTGdldz0=; _ir=0"
+		}
+			}).then(({data}) => {
+                const $ = cheerio.load(data)
+                const result = [];
+		        const hasil = [];
+                $('div > a').get().map(b => {
+                    const link = $(b).find('img').attr('src')
+                        result.push(link)
+                    });
+                       result.forEach(v => {
+                     if(v == undefined) return
+                     hasil.push(v.replace(/236/g,'736'))
+                        })
+                        hasil.shift();
+                        const dta = (random_item(hasil))
+                        const start = async function(a, b) {
+                            try {
+                                const media = await MessageMedia.fromUrl(dta)
+                                const chat = await msg.getChat()
+                                await chat.sendMessage(media, {caption: link})
+                            }catch (err){
+                                try{
+                                    msg.reply(`tidak menemukan ${link}`)
+                                }catch (err) {
+                                    console.log('')
+                                }
+                            }
+                        }
+                        start();
+            }).catch (err => {
+                try{
+                    msg.reply(`opss terjadi error`)
                 }catch (err) {
-                    console.log(err)
+                    console.log('')
                 }
-            }
-          }
+            })
     }
 });
-
 
 client.on('message', async (msg) => {
     if(msg.body.startsWith("!tr ")) {
