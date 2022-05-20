4da98ea71a88fe969bb9e82193fc571b45c70f7c
Reyz <91467569+user133883@users.noreply.github.com>
2022-05-21 00:16:57 +0800

    kenaffah

---
+++ func.js
@@ -0,0 +1,130 @@
+const cheerio = require("cheerio")
+const axios = require("axios")
+
+
+function youtube(link){
+	return new Promise((resolve, reject) => {
+		const ytIdRegex = /(?:http(?:s|):\/\/|)(?:(?:www\.|)youtube(?:\-nocookie|)\.com\/(?:watch\?.*(?:|\&)v=|embed\/|v\/)|youtu\.be\/)([-_0-9A-Za-z]{11})/
+		if (ytIdRegex.test(link)) {
+		let url =  ytIdRegex.exec(link)
+		let config = {
+			'url': 'https://www.youtube.be/' + url,
+			'q_auto': 0,
+			'ajax': 1
+		}
+		let headerss = 	{
+			"sec-ch-ua": '" Not;A Brand";v="99", "Google Chrome";v="91", "Chromium";v="91"',
+			"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+			"Cookie": 'PHPSESSID=6jo2ggb63g5mjvgj45f612ogt7; _ga=GA1.2.405896420.1625200423; _gid=GA1.2.2135261581.1625200423; _PN_SBSCRBR_FALLBACK_DENIED=1625200785624; MarketGidStorage={"0":{},"C702514":{"page":5,"time":1625200846733}}'
+		}
+	axios('https://www.y2mate.com/mates/en68/analyze/ajax',{
+			method: 'POST',
+			data: new URLSearchParams(Object.entries(config)),
+			headers: headerss
+		})
+	.then(({ data }) => {
+		const $ = cheerio.load(data.result)
+		let img = $('div.thumbnail.cover > a > img').attr('src');
+		let title = $('div.thumbnail.cover > div > b').text();
+		let size = $('#mp4 > table > tbody > tr:nth-child(3) > td:nth-child(2)').text()
+		let size_mp3 = $('#audio > table > tbody > tr:nth-child(1) > td:nth-child(2)').text()
+		let id = /var k__id = "(.*?)"/.exec(data.result)[1]
+		let configs = {
+    type: 'youtube',
+    _id: id,
+    v_id: url[1],
+    ajax: '1',
+    token: '',
+    ftype: 'mp4',
+    fquality: 480
+  }
+	axios('https://www.y2mate.com/mates/en68/convert',{
+		method: 'POST',
+		data: new URLSearchParams(Object.entries(configs)),
+		headers: headerss 
+	})
+	.then(({data}) => {
+		const $ = cheerio.load(data.result)
+		let link = $('div > a').attr('href')
+	let configss = {
+    type: 'youtube',
+    _id: id,
+    v_id: url[1],
+    ajax: '1',
+    token: '',
+    ftype: 'mp3',
+    fquality: 128
+  }
+	axios('https://www.y2mate.com/mates/en68/convert',{
+		method: 'POST',
+		data: new URLSearchParams(Object.entries(configss)),
+		headers: headerss 
+	})
+	.then(({ data }) => {
+		const $ = cheerio.load(data.result)
+		let audio = $('div > a').attr('href')
+		resolve({
+			id: url[1],
+			title: title,
+			size: size,
+			quality: '480p',
+			thumb: img,
+			link: link,
+			size_mp3: size_mp3,
+			mp3: audio
+		})
+
+		})
+			})
+		})
+	.catch(reject)
+	}else reject('link invalid')
+	})
+}
+
+function chara(query) {
+	return new Promise((resolve, reject) => {
+		axios.get('https://www.wallpaperflare.com/search?wallpaper='+ query,{
+			headers: {
+				"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+				"cookie": "_ga=GA1.2.863074474.1624987429; _gid=GA1.2.857771494.1624987429; __gads=ID=84d12a6ae82d0a63-2242b0820eca0058:T=1624987427:RT=1624987427:S=ALNI_MaJYaH0-_xRbokdDkQ0B49vSYgYcQ"
+			}
+		})
+		.then(({ data }) => {
+			const $ = cheerio.load(data)
+			const result = [];
+			$('#gallery > li > figure > a').each(function(a, b) {
+				result.push($(b).find('img').attr('data-src'))
+			})
+			resolve(result)
+		})
+	.catch({status: 'err'})
+	})
+}
+
+function lirik(judul){
+	return new Promise(async(resolve, reject) => {
+   		axios.get('https://www.musixmatch.com/search/' + judul)
+   		.then(async({ data }) => {
+   		const $ = cheerio.load(data)
+   		const hasil = {};
+   		let limk = 'https://www.musixmatch.com'
+   		const link = limk + $('div.media-card-body > div > h2').find('a').attr('href')
+	   		await axios.get(link)
+	   		.then(({ data }) => {
+		   		const $$ = cheerio.load(data)
+		   		hasil.thumb = 'https:' + $$('div.col-sm-1.col-md-2.col-ml-3.col-lg-3.static-position > div > div > div').find('img').attr('src')
+		  		$$('div.col-sm-10.col-md-8.col-ml-6.col-lg-6 > div.mxm-lyrics').each(function(a,b) {
+		   hasil.lirik = $$(b).find('span > p > span').text() +'\n' + $$(b).find('span > div > p > span').text()
+		   })
+	   })
+	   resolve(hasil)
+   })
+   .catch(reject)
+   })
+}
+
+
+module.exports.youtube = youtube
+module.exports.chara = chara
+module.exports.lirik = lirik
