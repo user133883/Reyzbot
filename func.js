ffb456c139a4bd7bd248bc2bd4d0e5dfe78fedff
Reyz <91467569+user133883@users.noreply.github.com>
2022-06-11 23:57:17 +0800

    Update func.js

--- func.js
+++ func.js
@@ -124,6 +124,51 @@ function lirik(judul){
    })
 }
 
+function google(query) {
+    return new Promise((resolve, reject) => {
+        const encodedString = encodeURI(query);
+        const AXIOS_OPTIONS = {
+            headers: {
+              "User-Agent":
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36",
+            },
+          };
+        axios.get(
+          `https://www.google.com/search?q=${encodedString}&hl=id&gl=us`,
+          AXIOS_OPTIONS
+        )
+        .then(function ({ data }) {
+          let $ = cheerio.load(data);
+    
+          const links = [];
+          const titles = [];
+          const snippets = [];
+    
+          $(".yuRUbf > a").each((i, el) => {
+            links[i] = $(el).attr("href");
+          });
+          $(".yuRUbf > a > h3").each((i, el) => {
+            titles[i] = $(el).text();
+          });
+          $(".IsZvec").each((i, el) => {
+            snippets[i] = $(el).text().trim();
+          });
+    
+          const result = [];
+          for (let i = 0; i < links.length; i++) {
+            result[i] = {
+              link: links[i],
+              title: titles[i],
+              snippet: snippets[i],
+            };
+          }
+          resolve(result)
+        })
+        .catch({status: 'err'})
+    })
+}
+
+
 function styletext(teks) {
     return new Promise((resolve, reject) => {
         axios.get('http://qaz.wtf/u/convert.cgi?text='+teks)
@@ -144,3 +189,4 @@ module.exports.styletext = styletext
 module.exports.youtube = youtube
 module.exports.chara = chara
 module.exports.lirik = lirik
+module.exports.google = google
